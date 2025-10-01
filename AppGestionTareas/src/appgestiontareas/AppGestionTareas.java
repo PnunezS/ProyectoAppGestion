53e709d52d430ff4681a983977f77ca6dc868cd3
PnunezS <PnunezS2401@educantabria.es>
2025-10-01 15:35:20 +0200

    Empezado base de main en AppGestionTareas
    Iniciado la programación del metodo main para recibir la elección del usuario

--- AppGestionTareas/src/appgestiontareas/AppGestionTareas.java
+++ AppGestionTareas/src/appgestiontareas/AppGestionTareas.java
@@ -4,6 +4,8 @@
  */
 package appgestiontareas;
 
+import java.util.Scanner;
+
 /**
  *
  * @author daw2
@@ -15,6 +17,44 @@ public class AppGestionTareas {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        int eleccion=0;
+        Scanner userInput=new Scanner(System.in);
+        
+        do{
+            printMenu();
+            System.out.println("Indique elección: ");
+            String textoEleccion=userInput.nextLine();
+            
+            if(textoEleccion.matches("[1-6]")){
+                //texto elección es solo numeros se puede parsear
+                System.out.println("Procesando elección");
+                eleccion=Integer.parseInt(textoEleccion);
+                
+            }else{
+                //No se puede parsear si no hay numeros
+                System.out.println("ERROR: solo se admite un numero de 1 a 6 para elegir");
+            }
+            
+        }while(eleccion!=6);
+    }
+    
+    //el programa debe permitir
+    // 1. crear nueva tarea
+    // 2. listar todas las tareas
+    // 3. completar tarea
+    // 4. eliminar tarea por indice
+    // 5. autoeliminar tareas completadas
+    // 6. cerrar
+    
+    private static void printMenu(){
+        System.out.println("***App gestión de tareas***");
+        System.out.println("   1. Crear nueva tarea");
+        System.out.println("   2. Listar todas las tareas");
+        System.out.println("   3. Marcar tarea de indice indicado como completada");
+        System.out.println("   4. Eliminar tarea de indice indicado");
+        System.out.println("   5. Autoeliminar todas las tareas marcadas como completadas");
+        System.out.println("   6. Salir");
     }
     
 }
