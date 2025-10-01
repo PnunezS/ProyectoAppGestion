b15d766150c16d37669f1602f6d8167f6738f2b3
PnunezS <PnunezS2401@educantabria.es>
2025-10-01 15:17:22 +0200

    Inicio de proyecto

---
+++ AppGestionTareas/src/appgestiontareas/AppGestionTareas.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package appgestiontareas;
+
+/**
+ *
+ * @author daw2
+ */
+public class AppGestionTareas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
